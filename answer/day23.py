32d21b0e37a134d916c27261aeb37b4007cf3dff
Jun-cloud <kjy8827@gmail.com>
2020-07-03 22:33:02 +0900

    Create day23

---
+++ answer/day23.py
@@ -0,0 +1,37 @@
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        # Write your code here
+        tempArr = [root] if root else []
+
+        while tempArr:
+            tempData = tempArr.pop()
+            print(tempData.data, end=" ")
+
+            if tempData.left: tempArr.insert(0, tempData.left)
+            if tempData.right: tempArr.insert(0, tempData.right)
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
